734ba63c7ab5c8adac1dbf5b862749cc45b24dfa
diixo <anatolii.podoba@gmail.com>
2024-06-03 11:16:04 +0300

    indexed_map.cpp update

--- indexed_map.cpp
+++ indexed_map.cpp
@@ -1,8 +1,14 @@
-
-#include <iostream>
+/*
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+*/
 #include <map>
 #include <vector>
 #include <functional>
+#include <algorithm>
+#include <stdexcept>
+
+//namespace py = pybind11;
 
 template<typename KeyType, typename ValueType>
 class CustomMap
@@ -28,12 +34,39 @@ public:
         return false;
     }
 
-    void print_insertion_order() const
-    {
-        for (size_t i = 0; i < keys.size(); ++i)
-        {
-            std::cout << keys[i] << ": " << values[i] << std::endl;
+    bool contains(const KeyType& key) const {
+        ValueType value;
+        return find(key, value);
+    }
+
+    KeyType get_key_by_index(size_t index) const {
+        if (index < keys.size()) {
+            return keys[index];
         }
+        throw std::out_of_range("Index out of range");
+    }
+
+    ValueType get_value_by_index(size_t index) const {
+        if (index < values.size()) {
+            return values[index];
+        }
+        throw std::out_of_range("Index out of range");
+    }
+
+    std::vector<std::pair<KeyType, ValueType>> get_insertion_order() const {
+        std::vector<std::pair<KeyType, ValueType>> result;
+        for (size_t i = 0; i < keys.size(); ++i) {
+            result.emplace_back(keys[i], values[i]);
+        }
+        return result;
+    }
+
+    bool __contains__(const KeyType& key) const {
+        return contains(key);
+    }
+
+    py::iterable keys() const {
+        return py::cast(keys);
     }
 
 private:
@@ -50,25 +83,28 @@ private:
         }
     };
 
-    std::map<size_t, size_t, Comparator> map{ Comparator(keys) };
-    std::vector<KeyType> keys;
-    std::vector<ValueType> values;
+    std::map <size_t, size_t, Comparator> map{ Comparator(keys) };
+    std::vector <KeyType> keys;
+    std::vector <ValueType> values;
 };
 
-int main()
+/*
+PYBIND11_MODULE(indexed_map, m)
 {
-    CustomMap<std::string, int> custom_map;
-    custom_map.insert("one", 1);
-    custom_map.insert("two", 2);
-    custom_map.insert("three", 3);
-
-    int value;
-    if (custom_map.find("two", value))
-    {
-        std::cout << "Found: " << value << std::endl;
-    }
-
-    custom_map.print_insertion_order();
-
-    return 0;
+    py::class_<CustomMap<std::string, int>>(m, "CustomMap")
+        .def(py::init<>())
+        .def("insert", &CustomMap<std::string, int>::insert)
+        .def("find", [](const CustomMap<std::string, int>& self, const std::string& key)
+        {
+            int value;
+            bool found = self.find(key, value);
+            return std::make_pair(found, value);
+        })
+        .def("contains", &CustomMap<std::string, int>::contains)
+        .def("get_key_by_index", &CustomMap<std::string, int>::get_key_by_index)
+        .def("get_value_by_index", &CustomMap<std::string, int>::get_value_by_index)
+        .def("get_insertion_order", &CustomMap<std::string, int>::get_insertion_order)
+        .def("__contains__", &CustomMap<std::string, int>::__contains__)
+        .def("__iter__", &CustomMap<std::string, int>::keys);
 }
+*/
